2e953e55bdef71b16a2a73ac26ac6ddbf9b5bd05
KrisDavie <kristofer.davie@gmail.com>
2025-05-12 11:28:01 +0200

    Version bump

--- js/util.js
+++ js/util.js
@@ -1,7 +1,7 @@
 (function (window) {
 	'use strict';
 
-	window.buildString = "20250413";
+	window.buildString = "20250512";
 
 	// based on https://github.com/medialize/URI.js/blob/gh-pages/src/URI.js
 	window.uri_query = memoize(function () {
